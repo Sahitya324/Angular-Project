f03516c1371306e25153889526ade29498dc5729
Sahitya324 <sahityakumar324@gmail.com>
2023-10-04 12:31:21 +0530

    Few schema added

--- src/app/template-list/model.ts
+++ src/app/template-list/model.ts
@@ -6,3 +6,11 @@ export interface templateModel{
   prompt: string,
   locale: string,
 }
+
+export interface templateListModal {
+  id: string,
+  title: string,
+  description: string,
+  created: string,
+  status: string,
+}
